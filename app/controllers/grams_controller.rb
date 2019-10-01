a864f568845320e7cf446c04774fb601d99ef357
Ivan Perez <ivan.perez002@gmail.com>
2019-10-01 04:15:31 +0000

    completed the development side of Lesson 16

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -2,6 +2,7 @@ class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
+    @grams = Gram.all
   end
 
   def new
@@ -55,6 +56,6 @@ class GramsController < ApplicationController
   end
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 end
